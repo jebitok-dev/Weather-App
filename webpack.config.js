faadb053a2c6642d2f25680d456035f705a59858
Jebitok <sharonkosgei4@gmail.com>
2021-05-12 13:00:27 +0300

    Add Initial Files

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+/*eslint-disable */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+/*eslint-enable */
